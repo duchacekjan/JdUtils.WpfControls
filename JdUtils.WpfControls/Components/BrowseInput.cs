4918de6f6d666294890d702e6dcf435694702189
Ducháček Jan <jan.duchacek.bc@gmail.com>
2020-07-16 11:07:51 +0200

    Úprava barev. Borderless textbox

--- JdUtils.WpfControls/Components/BrowseInput.cs
+++ JdUtils.WpfControls/Components/BrowseInput.cs
@@ -5,11 +5,19 @@ namespace JdUtils.WpfControls.Components
 {
     public class BrowseInput : Control
     {
+        public static readonly DependencyProperty UserCanWriteProperty;
 
         static BrowseInput()
         {
             var owner = typeof(BrowseInput);
+            UserCanWriteProperty = DependencyProperty.Register(nameof(UserCanWrite), typeof(bool), owner, new FrameworkPropertyMetadata());
             DefaultStyleKeyProperty.OverrideMetadata(owner, new FrameworkPropertyMetadata(owner));
+        }
+
+        public bool UserCanWrite
+        {
+            get => (bool)GetValue(UserCanWriteProperty);
+            set => SetValue(UserCanWriteProperty, value);
         }
     }
 }
