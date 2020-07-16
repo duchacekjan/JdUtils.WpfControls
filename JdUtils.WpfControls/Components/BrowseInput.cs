65cdcdcc5762f4c883b1fe60def061f7c055e04a
Ducháček Jan <jan.duchacek.bc@gmail.com>
2020-07-16 10:39:29 +0200

    Vytvořen základ.

---
+++ JdUtils.WpfControls/Components/BrowseInput.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace JdUtils.WpfControls.Components
+{
+    public class BrowseInput : Control
+    {
+
+        static BrowseInput()
+        {
+            var owner = typeof(BrowseInput);
+            DefaultStyleKeyProperty.OverrideMetadata(owner, new FrameworkPropertyMetadata(owner));
+        }
+    }
+}
