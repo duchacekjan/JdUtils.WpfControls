e581ed9714c29dec178975a40392106058165609
Ducháček Jan <jan.duchacek.bc@gmail.com>
2020-07-24 06:24:30 +0200

    Vytvoření základu komponenty

---
+++ JdUtils.WpfControls/Components/LoginView.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace JdUtils.WpfControls.Components
+{
+    public class LoginView : Control
+    {
+        static LoginView()
+        {
+            var owner = typeof(LoginView);
+            DefaultStyleKeyProperty.OverrideMetadata(owner, new FrameworkPropertyMetadata(owner));
+        }   
+    }
+}
