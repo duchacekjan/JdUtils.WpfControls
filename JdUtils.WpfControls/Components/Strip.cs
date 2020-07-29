bff37d034518ca98d30bba2e46db39d3ffba8e56
Ducháček Jan <jan.duchacek.bc@gmail.com>
2020-07-29 12:05:46 +0200

    Povýšení nugetu. Základ komponenty

---
+++ JdUtils.WpfControls/Components/Strip.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace JdUtils.WpfControls.Components
+{
+    public class Strip : Control
+    {
+        static Strip()
+        {
+            var owner = typeof(Strip);
+            DefaultStyleKeyProperty.OverrideMetadata(owner, new FrameworkPropertyMetadata(owner));
+        }
+    }
+}
