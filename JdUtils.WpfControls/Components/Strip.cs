080fc2cf36facd53fc72eb80c6d5f5fbfa2c28fa
Ducháček Jan <jan.duchacek.bc@gmail.com>
2020-07-30 10:19:51 +0200

    Implementace základního přepínání stripu

--- JdUtils.WpfControls/Components/Strip.cs
+++ JdUtils.WpfControls/Components/Strip.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 
 namespace JdUtils.WpfControls.Components
 {
@@ -12,16 +13,31 @@ namespace JdUtils.WpfControls.Components
         public const string PartImage = "PART_Image";
         public static readonly DependencyProperty LinkProperty;
         public static readonly DependencyProperty CountProperty;
+        public static readonly DependencyProperty IntervalProperty;
+
+        private readonly DispatcherTimer m_timer;
 
         private ImageBrush m_image;
         private BitmapImage m_bitmap;
         private int m_current;
+
         static Strip()
         {
             var owner = typeof(Strip);
             LinkProperty = DependencyProperty.Register(nameof(Link), typeof(string), owner, new FrameworkPropertyMetadata(string.Empty, OnLinkChangedCallback));
             CountProperty = DependencyProperty.Register(nameof(Count), typeof(int), owner, new FrameworkPropertyMetadata(0, OnCountChangedCallback));
+            IntervalProperty = DependencyProperty.Register(nameof(Interval), typeof(int), owner, new FrameworkPropertyMetadata(1500, OnIntervalChangedCallback));
             DefaultStyleKeyProperty.OverrideMetadata(owner, new FrameworkPropertyMetadata(owner));
+        }
+
+        public Strip()
+        {
+            m_timer = new DispatcherTimer(DispatcherPriority.Background)
+            {
+                Interval = TimeSpan.FromMilliseconds(Interval),
+                IsEnabled = false
+            };
+            m_timer.Tick += OnTimer;
         }
 
         public int Count
@@ -36,11 +52,23 @@ namespace JdUtils.WpfControls.Components
             set => SetValue(LinkProperty, value);
         }
 
+        public int Interval
+        {
+            get => (int)GetValue(IntervalProperty);
+            set => SetValue(IntervalProperty, value);
+        }
+
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
-            m_image = this.FindTemplatePart<ImageBrush>(PartImage);
+            m_image = this.FindTemplatePart<ImageBrush>(PartImage)
+                .AndIfNotNull(i =>
+                {
+                    i.Viewbox = new Rect(0, 0, 1, 1);
+                    i.ViewboxUnits = BrushMappingMode.RelativeToBoundingBox;
+                });
             UpdateImage();
+            OnIntervalChanged();
         }
 
         private static void OnLinkChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -65,9 +93,29 @@ namespace JdUtils.WpfControls.Components
             }
         }
 
+        private static void OnIntervalChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Strip control)
+            {
+                if (!Equals(e.OldValue, e.NewValue))
+                {
+                    control.OnIntervalChanged();
+                }
+            }
+        }
+
+        private void OnIntervalChanged()
+        {
+            m_timer.Stop();
+            m_timer.Interval = TimeSpan.FromMilliseconds(Interval);
+            if (Interval > 0)
+            {
+                m_timer.Start();
+            }
+        }
+
         private void OnCountChanged()
         {
-            m_current = 0;
             UpdateImage();
         }
 
@@ -85,12 +133,12 @@ namespace JdUtils.WpfControls.Components
 
         private void UpdateImage()
         {
+            m_current = 0;
             m_image.SetValueSafe(s => s.ImageSource, m_bitmap);
             if (m_bitmap != null && Count > 0)
             {
                 var relativeWidth = 1.0 / Count;
                 m_image.SetValueSafe(s => s.Viewbox, new Rect(m_current * relativeWidth, 0, relativeWidth, 1));
-                m_image.SetValueSafe(s => s.ViewboxUnits, BrushMappingMode.RelativeToBoundingBox);
                 Visibility = Visibility.Visible;
             }
             else
@@ -98,5 +146,57 @@ namespace JdUtils.WpfControls.Components
                 Visibility = Visibility.Collapsed;
             }
         }
+
+        private void OnTimer(object sender, EventArgs e)
+        {
+            if (m_bitmap != null && m_image != null && Count > 0)
+            {
+                if (m_current >= Count)
+                {
+                    m_current = 0;
+                }
+
+                var relativeWidth = 1.0 / Count;
+                //TODO Cast inspirace pro SlideShow
+                //if (m_current > 0)
+                //{
+                //    Slide(m_current - 1, m_current, relativeWidth);
+                //}
+                //else
+                //{
+                //    m_image.Viewbox = new Rect(m_current * relativeWidth, 0, relativeWidth, 1);
+                //}
+                m_image.Viewbox = new Rect(m_current * relativeWidth, 0, relativeWidth, 1);
+                m_current++;
+            }
+        }
+
+        //TODO Inspirace pro SlideShow
+        //private DispatcherTimer m_slider = new DispatcherTimer(DispatcherPriority.Render);
+        //private double m_step;
+        //private double m_end;
+        //private double m_w;
+
+        //private void Slide(double start, double end, double relativeWidth)
+        //{
+        //    m_slider.Stop();
+        //    m_slider.Interval = TimeSpan.FromMilliseconds(100);
+        //    m_slider.Tick -= OnSlide;
+        //    m_slider.Tick += OnSlide;
+        //    m_step = start;
+        //    m_w = relativeWidth;
+        //    m_end = end;
+        //    m_slider.Start();
+        //}
+
+        //private void OnSlide(object sender, EventArgs e)
+        //{
+        //    m_step+=m_w/10;
+        //    m_image.Viewbox = new Rect(m_step * m_w, 0, m_w, 1);
+        //    if (m_step >= m_end)
+        //    {
+        //        m_timer.Stop();
+        //    }
+        //}
     }
 }
