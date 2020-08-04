d1da62572b032c1d6c86b9bf208ef3ccd332f643
Ducháček Jan <jan.duchacek.bc@gmail.com>
2020-08-04 07:39:51 +0200

    Připraven ComboBox pro výběr enumů. Chybí povýšení JdUtils

---
+++ JdUtils.WpfControls/Components/EnumValueSelector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace JdUtils.WpfControls.Components
+{
+    public class EnumValueSelector : ComboBox
+    {
+        public static readonly DependencyProperty EnumTypeProperty;
+
+        static EnumValueSelector()
+        {
+            var owner = typeof(EnumValueSelector);
+            EnumTypeProperty = DependencyProperty.Register(nameof(EnumType), typeof(Type), owner, new FrameworkPropertyMetadata());
+        }
+
+        public Type EnumType
+        {
+            get => (Type)GetValue(EnumTypeProperty);
+            set => SetValue(EnumTypeProperty, value);
+        }
+    }
+}
