1f2c8a9bc17734d0cd720c7d55c358fe191ac300
JGS <jan.duchacek.bc@gmail.com>
2020-11-11 11:50:09 +0100

    Lazení vzhledu

--- JdUtils.WpfControls/Components/PathButton.cs
+++ JdUtils.WpfControls/Components/PathButton.cs
@@ -7,20 +7,28 @@ namespace JdUtils.WpfControls.Components
     public class PathButton : Button
     {
         public static readonly DependencyProperty PathProperty;
-        public static readonly DependencyProperty TextPositionProperty;
+        public static readonly DependencyProperty IconPositionProperty;
+        public static readonly DependencyProperty IconSizeProperty;
 
         static PathButton()
         {
             var owner = typeof(PathButton);
             PathProperty = DependencyProperty.Register(nameof(Path), typeof(Path), owner, new FrameworkPropertyMetadata());
-            TextPositionProperty = DependencyProperty.Register(nameof(TextPosition), typeof(Dock), owner, new FrameworkPropertyMetadata());
+            IconPositionProperty = DependencyProperty.Register(nameof(IconPosition), typeof(Dock), owner, new FrameworkPropertyMetadata(Dock.Bottom));
+            IconSizeProperty = DependencyProperty.Register(nameof(IconSize), typeof(int), owner, new FrameworkPropertyMetadata(16));
             DefaultStyleKeyProperty.OverrideMetadata(owner, new FrameworkPropertyMetadata(owner));
         }
 
-        public Dock TextPosition
+        public int IconSize
         {
-            get => (Dock)GetValue(TextPositionProperty);
-            set => SetValue(TextPositionProperty, value);
+            get => (int)GetValue(IconSizeProperty);
+            set => SetValue(IconSizeProperty, value);
+        }
+
+        public Dock IconPosition
+        {
+            get => (Dock)GetValue(IconPositionProperty);
+            set => SetValue(IconPositionProperty, value);
         }
 
         public Path Path
