b467cf46ca91b0e7ff720ee23ca83df7313b057c
JGS <jan.duchacek.bc@gmail.com>
2020-11-11 11:06:14 +0100

    Unsupported net 462, new properties

--- JdUtils.WpfControls/Components/PathButton.cs
+++ JdUtils.WpfControls/Components/PathButton.cs
@@ -7,12 +7,20 @@ namespace JdUtils.WpfControls.Components
     public class PathButton : Button
     {
         public static readonly DependencyProperty PathProperty;
+        public static readonly DependencyProperty TextPositionProperty;
 
         static PathButton()
         {
             var owner = typeof(PathButton);
             PathProperty = DependencyProperty.Register(nameof(Path), typeof(Path), owner, new FrameworkPropertyMetadata());
+            TextPositionProperty = DependencyProperty.Register(nameof(TextPosition), typeof(Dock), owner, new FrameworkPropertyMetadata());
             DefaultStyleKeyProperty.OverrideMetadata(owner, new FrameworkPropertyMetadata(owner));
+        }
+
+        public Dock TextPosition
+        {
+            get => (Dock)GetValue(TextPositionProperty);
+            set => SetValue(TextPositionProperty, value);
         }
 
         public Path Path
