20920bd1c09c60a7f6c1b89527416583f2a490e1
JGS <jan.duchacek.bc@gmail.com>
2021-01-04 14:16:57 +0100

    Základní příprava. Resizable text. Tlačítko pro vypnutí. Contrast color converter

---
+++ JdUtils.WpfControls/Components/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace JdUtils.WpfControls.Components
+{
+    public class Tag : Control
+    {
+
+        static Tag()
+        {
+            var owner = typeof(Tag);
+            DefaultStyleKeyProperty.OverrideMetadata(owner, new FrameworkPropertyMetadata(owner));
+        }
+    }
+}
