8b5631be6d439984426cc186e5bb998befac70af
JGS <jan.duchacek.bc@gmail.com>
2021-01-05 07:09:39 +0100

    Přidání commandu

--- JdUtils.WpfControls/Components/Tag.cs
+++ JdUtils.WpfControls/Components/Tag.cs
@@ -1,15 +1,68 @@
-﻿using System.Windows;
+﻿using JdUtils.Extensions;
+using JdUtils.WpfControls.Utils;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace JdUtils.WpfControls.Components
 {
     public class Tag : Control
     {
+        public const string PartCloseButton = "PART_CloseButton";
+        public const string PartBorder = "PART_Border";
+        public static readonly DependencyProperty CommandProperty;
+        private bool m_buttonDown;
 
         static Tag()
         {
             var owner = typeof(Tag);
+            CommandProperty = DependencyProperty.Register(nameof(Command), typeof(ICommand), owner, new FrameworkPropertyMetadata());
             DefaultStyleKeyProperty.OverrideMetadata(owner, new FrameworkPropertyMetadata(owner));
+        }
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            this.FindTemplatePart<Button>(PartCloseButton)
+                .IfNotNull(b =>
+                {
+                    b.Click += (s, e) => InvokeCmd(TagCommmandSource.CloseButton);
+                });
+            this.FindTemplatePart<Border>(PartBorder)
+                .IfNotNull(b =>
+                {
+                    b.MouseDown += OnBorderMouseDown;
+                    b.MouseUp += OnBorderMouseUp;
+                });
+        }
+
+        private void OnBorderMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.MiddleButton == MouseButtonState.Released && m_buttonDown)
+            {
+                m_buttonDown = false;
+                InvokeCmd(TagCommmandSource.Tag);
+            }
+        }
+
+        private void OnBorderMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            m_buttonDown = e.MiddleButton == MouseButtonState.Pressed;
+        }
+
+        private void InvokeCmd(TagCommmandSource source)
+        {
+            var args = new TagCommand(this, source);
+            if (Command?.CanExecute(args) == true)
+            {
+                Command.Execute(args);
+            }
         }
     }
 }
---
+++ JdUtils.WpfControls/Utils/TagCommand.cs
@@ -0,0 +1,23 @@
+﻿using JdUtils.WpfControls.Components;
+
+namespace JdUtils.WpfControls.Utils
+{
+    public enum TagCommmandSource
+    {
+        CloseButton,
+        Tag
+    }
+
+    public class TagCommand
+    {
+        public TagCommand(Tag sender, TagCommmandSource source)
+        {
+            Sender = sender;
+            Source = source;
+        }
+
+        public Tag Sender { get; }
+
+        public TagCommmandSource Source { get; }
+    }
+}
