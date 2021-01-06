29a6dc60e14707ecbb4c0cee7a7e6b89cb1b20a4
JGS <jan.duchacek.bc@gmail.com>
2021-01-06 07:02:35 +0100

    Podpora přímého contentu. Testy se zobrazením

--- JdUtils.WpfControls/Components/TagControl.cs
+++ JdUtils.WpfControls/Components/TagControl.cs
@@ -1,18 +1,35 @@
 ﻿using JdUtils.WpfControls.Utils;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Markup;
 
 namespace JdUtils.WpfControls.Components
 {
+    [ContentProperty(nameof(Items))]
     public class TagControl : Control
     {
         public static readonly DependencyProperty ItemsSourceProperty;
+        public static readonly DependencyProperty ItemsProperty;
+
         static TagControl()
         {
             var owner = typeof(TagControl);
             ItemsSourceProperty = DependencyProperty.Register(nameof(ItemsSource), typeof(IEnumerable<ITag>), owner, new FrameworkPropertyMetadata());
+            ItemsProperty = DependencyProperty.Register(nameof(Items), typeof(ObservableCollection<Tag>), owner, new UIPropertyMetadata());
             DefaultStyleKeyProperty.OverrideMetadata(owner, new FrameworkPropertyMetadata(owner));
+        }
+
+        public TagControl()
+        {
+            Items = new ObservableCollection<Tag>();
+        }
+
+        public ObservableCollection<Tag> Items
+        {
+            get => (ObservableCollection<Tag>)GetValue(ItemsProperty);
+            set => SetValue(ItemsProperty, value);
         }
 
         public IEnumerable<ITag> ItemsSource
