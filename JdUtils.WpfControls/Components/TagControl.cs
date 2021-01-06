c6a29c2da2ab77b6a382fa170b5582c21c053ed4
JGS <jan.duchacek.bc@gmail.com>
2021-01-06 06:32:58 +0100

    Kostra tag control

---
+++ JdUtils.WpfControls/Components/TagControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace JdUtils.WpfControls.Components
+{
+    public class TagControl : Control
+    {
+        static TagControl()
+        {
+            var owner = typeof(TagControl);
+            DefaultStyleKeyProperty.OverrideMetadata(owner, new FrameworkPropertyMetadata(owner));
+        }
+    }
+}
